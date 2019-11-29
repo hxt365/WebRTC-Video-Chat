619352fa3761fbf710c3a53592d3f587703fc3bd
Truong Hoang <hxt365@gmail.com>
2019-11-29 22:05:01 +0300

    Deprecate addstream

--- README.md
+++ README.md
@@ -1,20 +1,21 @@
-This is a basic one to one video chat web app, powered by WebRTC and Firebase
+This is a basic one to one video chat web app, implemented with WebRTC and Firebase
+
 # TODO
-1. Register a free stun server at http://numb.viagenie.ca/
+
+1. Register a free turn server at http://numb.viagenie.ca/
 2. Register a firebase and allow read & write permissions
 3. Add a .env file. This file looks like:
-REACT_APP_API_KEY=********************
-REACT_APP_AUTH_DOMAIN=******.firebaseapp.com
-REACT_APP_DATABASE_URL=https://*******.firebaseio.com
-REACT_APP_PROJECT_ID=********
-REACT_APP_STORAGE_BUCKET=*******.appspot.com
-REACT_APP_MESSAGING_SENDER_ID=***********
-REACT_APP_APP_ID=****************
-REACT_APP_MEASUREMENT_ID=********
-
-REACT_APP_STUN_MOZZILA_URL=stun:stun.services.mozilla.com
-REACT_APP_STUN_GOOGLE_URL=stun:stun.l.google.com:19302
-REACT_APP_TURN_URL=turn:numb.viagenie.ca
-REACT_APP_TURN_USERNAME=**********
-REACT_APP_TURN_CREDENTIAL=*********
+   REACT_APP_API_KEY=**
+   REACT_APP_AUTH_DOMAIN=**
+   REACT_APP_DATABASE_URL=https://**.firebaseio.com
+   REACT_APP_PROJECT_ID=**
+   REACT_APP_STORAGE_BUCKET=**.appspot.com
+   REACT_APP_MESSAGING_SENDER_ID=**
+   REACT_APP_APP_ID=**
+   REACT_APP_MEASUREMENT_ID=**
+   REACT_APP_STUN_MOZZILA_URL=stun:stun.services.mozilla.com
+   REACT_APP_STUN_GOOGLE_URL=stun:stun.l.google.com:19302
+   REACT_APP_TURN_URL=turn:numb.viagenie.ca
+   REACT_APP_TURN_USERNAME=**
+   REACT_APP_TURN_CREDENTIAL=**
 4. npm start
--- src/views/homepage/index.js
+++ src/views/homepage/index.js
@@ -10,7 +10,6 @@ function Homepage() {
   const readMessage = async data => {
     const msg = JSON.parse(data.val().message);
     const sender = { ...data.val() };
-    // console.log(msg);
     if (sender !== yourId) {
       if (msg.ice) pc.addIceCandidate(new RTCIceCandidate(msg.ice));
       else if (msg.sdp.type === 'offer') {
@@ -24,27 +23,38 @@ function Homepage() {
     }
   };
 
-  useEffect(() => {
-    pc.onicecandidate = event => {
-      if (event.candidate) sendMessage({ ice: event.candidate });
-      else console.log('Sent all ice');
-    };
+  pc.onicecandidate = event => {
+    if (event.candidate) sendMessage({ ice: event.candidate });
+    else console.log('Sent all ice');
+  };
 
-    pc.onaddstream = event => {
-      setRemoteStream(event.stream);
-    };
-
-    fb.on('child_added', readMessage);
-  }, []);
+  pc.ontrack = event => {
+    if (event.streams && event.streams[0]) {
+      setRemoteStream(event.streams[0]);
+    } else {
+      let inboundStream = null;
+      if (!remoteStream) {
+        inboundStream = new MediaStream();
+        inboundStream.addTrack(event.track);
+      } else {
+        inboundStream = { ...remoteStream };
+        inboundStream.addTrack(event.track);
+      }
+      setRemoteStream(inboundStream);
+    }
+  };
 
   useEffect(() => {
     const showMyFace = async () => {
       const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
-      pc.addStream(stream);
+      for (const track of stream.getTracks()) {
+        pc.addTrack(track, stream);
+      }
       setLocalStream(stream);
     };
 
     showMyFace();
+    fb.on('child_added', readMessage);
   }, []);
 
   const showFriendsFace = async () => {
